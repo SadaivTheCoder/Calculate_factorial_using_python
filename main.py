b9ea72facbc6a4a3c68df5b448b091037b0d39ca
SadaivTheCoder <Sadaivrajkeshri@gmail.com>
2025-04-23 20:13:04 +0530

    Added factorial functions with recursive logic
    Created a function 'facto n' to calculate factorial using recursion

---
+++ main.py
@@ -0,0 +1,9 @@
+# Write a program to calculate the factorial of number n:->
+def facto(n):
+    if(n==0):
+        return 1
+    else:
+        return n*facto(n-1)
+n = int(input("Enter a number:-> ")
+print(facto(n))
+
